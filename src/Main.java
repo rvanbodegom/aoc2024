4cbd0224ab44061956dfe5afc7b28a11f968d6c4
Ruben van Bodegom <ruben@rubyit.nl>
2024-12-06 15:06:29 +0100

    Day 12a

--- src/Main.java
+++ src/Main.java
@@ -37,8 +37,7 @@ public class Main {
         System.out.println(results);
       }
     } catch (IOException e) {
-      e.printStackTrace();
-    }
+      e.printStackTrace();    }
   }
 
   private static void calculateAndCompareContiquousGroups(String springLayout) {
