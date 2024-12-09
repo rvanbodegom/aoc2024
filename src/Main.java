8a82ffd08d09700027a801f42c3754237c2c28bb
Ruben van Bodegom <ruben@rubyit.nl>
2024-12-09 15:03:18 +0100

    Day 14b

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
   static char roundedRock = 'O';
   static char cubeRock = '#';
   static char empty = '.';
-  static int results = 0;
+  static long results = 0;
+  static long nrOfRotations = 1000;
 
   public static void main(String[] args) {
     String filePath = "src/input.txt";
@@ -18,7 +20,7 @@ public class Main {
     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
       String line;
       List<String> fullPuzzleInput = new ArrayList<>();
-      List<String> shiftedPuzzleInput = new ArrayList<>();
+      HashMap<Integer, List<String>> shiftedPuzzleInput = new HashMap<>();
       while ((line = br.readLine()) != null) {
         if (!line.isEmpty()) {
           fullPuzzleInput.add(line);
@@ -26,28 +28,122 @@ public class Main {
         }
       }
       //fullPuzzleInput.removeLast(); // remove last empty line
-      fullPuzzleInput = invertMatrix(fullPuzzleInput);
-      // now shift left all roundedRocks until they hit either another roundedRock or a cubeRock
-      for (String singleLine : fullPuzzleInput) {
-        char[] chars = singleLine.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-          if (chars[i] == roundedRock) {
-            int j = i;
-            while (j > 0 && chars[j - 1] == empty) {
-              chars[j - 1] = roundedRock;
-              chars[j] = empty;
-              j--;
+      for (int r = 0; r < 4 * nrOfRotations; r++) {
+//        fullPuzzleInput = invertMatrix(fullPuzzleInput);
+//        List<String> singleLineList = new ArrayList<>();
+        if (r%100000 == 0) {
+          System.out.println("r: " + r);
+        }
+
+//        System.out.println("fpi:" + fullPuzzleInput);
+        if (r%4 == 0) {
+//          System.out.println("UP");
+          // now shift up all roundedRocks until they hit either another roundedRock or a cubeRock
+          for (int col = 0; col < fullPuzzleInput.get(0).length(); col++) {
+            for (int row = 0; row < fullPuzzleInput.size(); row++) {
+              char[] chars = fullPuzzleInput.get(row).toCharArray();
+              if (chars[col] == roundedRock) {
+                int k = row;
+                while (k > 0 && fullPuzzleInput.get(k - 1).charAt(col) == empty) {
+                  chars = fullPuzzleInput.get(k).toCharArray();
+                  char[] aboveChars = fullPuzzleInput.get(k - 1).toCharArray();
+                  aboveChars[col] = roundedRock;
+                  chars[col] = empty;
+                  fullPuzzleInput.set(k, new String(chars));
+                  fullPuzzleInput.set(k - 1, new String(aboveChars));
+                  k--;
+                }
+              }
             }
           }
+//          for (String inputLine : fullPuzzleInput) {
+//            System.out.println(inputLine);
+//          }
         }
-        results += calculateSingleLineWeight(chars);
-        singleLine = Arrays.toString(chars);
-        shiftedPuzzleInput.add(singleLine);
-        System.out.println(singleLine);
-        System.out.println("results: " + results);
+        // now shift left all roundedRocks until they hit either another roundedRock or a cubeRock
+        if (r%4 == 1) {
+//          System.out.println("LEFT");
+          for (int l = 0; l < fullPuzzleInput.size(); l++) {
+            String singleLine = fullPuzzleInput.get(l);
+            char[] chars = singleLine.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+              if (chars[i] == roundedRock) {
+                int j = i;
+                while (j > 0 && chars[j - 1] == empty) {
+                  chars[j - 1] = roundedRock;
+                  chars[j] = empty;
+                  j--;
+                }
+              }
+            }
+            fullPuzzleInput.set(l, new String(chars));
+          }
+//          for (String inputLine : fullPuzzleInput) {
+//            System.out.println(inputLine);
+//          }
+        }
+
+        if (r%4 == 2) { // SOUTH
+
+//          System.out.println("SOUTH");
+          // now shift down all roundedRocks until they hit either another roundedRock or a cubeRock
+
+          for (int col = 0; col < fullPuzzleInput.get(0).length(); col++) {
+            for (int row = fullPuzzleInput.size() - 1; row >= 0; row--) {
+              char[] chars = fullPuzzleInput.get(row).toCharArray();
+              if (chars[col] == roundedRock) {
+                int k = row;
+                while (k < fullPuzzleInput.get(0).length() - 1 && fullPuzzleInput.get(k + 1).charAt(col) == empty) {
+                  chars = fullPuzzleInput.get(k).toCharArray();
+                  char[] belowChars = fullPuzzleInput.get(k + 1).toCharArray();
+                  belowChars[col] = roundedRock;
+                  chars[col] = empty;
+                  fullPuzzleInput.set(k, new String(chars));
+                  fullPuzzleInput.set(k + 1, new String(belowChars));
+                  k++;
+                }
+              }
+            }
+          }
+//          for (String inputLine : fullPuzzleInput) {
+//            System.out.println(inputLine);
+//          }
+        }
+
+        // now shift right all roundedRocks until they hit either another roundedRock or a cubeRock
+        if (r%4 == 3) {
+//          System.out.println("RIGHT");
+          for (int l = 0; l < fullPuzzleInput.size(); l++) {
+            String singleLine = fullPuzzleInput.get(l);
+            char[] chars = singleLine.toCharArray();
+            for (int i = chars.length - 1; i >= 0; i--) {
+              if (chars[i] == roundedRock) {
+                int j = i;
+                while (j < chars.length - 1 && chars[j + 1] == empty) {
+                  chars[j + 1] = roundedRock;
+                  chars[j] = empty;
+                  j++;
+                }
+              }
+            }
+            fullPuzzleInput.set(l, new String(chars));
+          }
+//          for (String inputLine : fullPuzzleInput) {
+//            System.out.println(inputLine);
+//          }
+        }
       }
 
-      System.out.println("shifted:" + shiftedPuzzleInput);
+      // DONE
+      System.out.println("DONE");
+      for (int i = 0; i < fullPuzzleInput.size(); i++) {
+        System.out.println("size: " + fullPuzzleInput.size());
+        String inputLine = fullPuzzleInput.get(i);
+        System.out.println(inputLine);
+        results += calculateSingleLineWeight(inputLine.toCharArray(), fullPuzzleInput.size()-i);
+      }
+      System.out.println("results: " + results);
+
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -57,34 +153,16 @@ public class Main {
    * This method should calculate the weight of a single line, the weight is calculated by taking the sum of the
    *  distance from each roundedRock to the end of the string
    */
-  public static int calculateSingleLineWeight(char[] chars) {
-    int weight = 0;
+  public static long calculateSingleLineWeight(char[] chars, int weight) {
+    long totalWeight = 0;
     for (int i = 0; i < chars.length; i++) {
       if (chars[i] == roundedRock) {
-        weight += chars.length - i;
-        System.out.println("weight: " + weight);
+        totalWeight += weight;
+//        System.out.println("weight: " + weight);
       }
     }
-    return weight;
+    return totalWeight;
   }
-
-  public static List<String> invertMatrix(List<String> input) {
-    int maxLength = input.stream().mapToInt(String::length).max().orElse(0);
-    List<String> inverted = new ArrayList<>();
-
-    for (int i = 0; i < maxLength; i++) {
-      StringBuilder sb = new StringBuilder();
-      for (String s : input) {
-        if (i < s.length()) {
-          sb.append(s.charAt(i));
-        } else {
-          sb.append(' '); // Fill with space if the string is shorter
-        }
-      }
-      inverted.add(sb.toString());
-    }
-
-    return inverted;
-  }
-
 }
+
+
