661b6343e264b704ad4cd2556a788118b71e0420
Ruben van Bodegom <ruben@rubyit.nl>
2024-12-10 16:16:59 +0100

    Day 15a

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
   static char roundedRock = 'O';
   static char cubeRock = '#';
   static char empty = '.';
-  static long results = 0;
+  static long result = 0;
   static long nrOfRotations = 1000;
 
   public static void main(String[] args) {
@@ -19,149 +19,34 @@ public class Main {
 
     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
       String line;
+      int hashResult = 0;
       List<String> fullPuzzleInput = new ArrayList<>();
       HashMap<Integer, List<String>> shiftedPuzzleInput = new HashMap<>();
       while ((line = br.readLine()) != null) {
         if (!line.isEmpty()) {
-          fullPuzzleInput.add(line);
-          System.out.println(line);
+          fullPuzzleInput.addAll(List.of(line.split(",")));
+          System.out.println(fullPuzzleInput);
         }
       }
-      //fullPuzzleInput.removeLast(); // remove last empty line
-      for (int r = 0; r < 4 * nrOfRotations; r++) {
-//        fullPuzzleInput = invertMatrix(fullPuzzleInput);
-//        List<String> singleLineList = new ArrayList<>();
-        if (r%100000 == 0) {
-          System.out.println("r: " + r);
-        }
+      for (String step : fullPuzzleInput) {
+        hashResult = 0;
+        char[] chars = step.toCharArray();
+        for (char c : chars) {
+          hashResult += (int) c;
+//          System.out.println("hashResult: " + hashResult);
+          hashResult *= 17;
+//          System.out.println("hashResult: " + hashResult);
+          hashResult = hashResult % 256;
+//          System.out.println("hashResult: " + hashResult);
 
-//        System.out.println("fpi:" + fullPuzzleInput);
-        if (r%4 == 0) {
-//          System.out.println("UP");
-          // now shift up all roundedRocks until they hit either another roundedRock or a cubeRock
-          for (int col = 0; col < fullPuzzleInput.get(0).length(); col++) {
-            for (int row = 0; row < fullPuzzleInput.size(); row++) {
-              char[] chars = fullPuzzleInput.get(row).toCharArray();
-              if (chars[col] == roundedRock) {
-                int k = row;
-                while (k > 0 && fullPuzzleInput.get(k - 1).charAt(col) == empty) {
-                  chars = fullPuzzleInput.get(k).toCharArray();
-                  char[] aboveChars = fullPuzzleInput.get(k - 1).toCharArray();
-                  aboveChars[col] = roundedRock;
-                  chars[col] = empty;
-                  fullPuzzleInput.set(k, new String(chars));
-                  fullPuzzleInput.set(k - 1, new String(aboveChars));
-                  k--;
-                }
-              }
-            }
-          }
-//          for (String inputLine : fullPuzzleInput) {
-//            System.out.println(inputLine);
-//          }
         }
-        // now shift left all roundedRocks until they hit either another roundedRock or a cubeRock
-        if (r%4 == 1) {
-//          System.out.println("LEFT");
-          for (int l = 0; l < fullPuzzleInput.size(); l++) {
-            String singleLine = fullPuzzleInput.get(l);
-            char[] chars = singleLine.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-              if (chars[i] == roundedRock) {
-                int j = i;
-                while (j > 0 && chars[j - 1] == empty) {
-                  chars[j - 1] = roundedRock;
-                  chars[j] = empty;
-                  j--;
-                }
-              }
-            }
-            fullPuzzleInput.set(l, new String(chars));
-          }
-//          for (String inputLine : fullPuzzleInput) {
-//            System.out.println(inputLine);
-//          }
-        }
-
-        if (r%4 == 2) { // SOUTH
-
-//          System.out.println("SOUTH");
-          // now shift down all roundedRocks until they hit either another roundedRock or a cubeRock
-
-          for (int col = 0; col < fullPuzzleInput.get(0).length(); col++) {
-            for (int row = fullPuzzleInput.size() - 1; row >= 0; row--) {
-              char[] chars = fullPuzzleInput.get(row).toCharArray();
-              if (chars[col] == roundedRock) {
-                int k = row;
-                while (k < fullPuzzleInput.get(0).length() - 1 && fullPuzzleInput.get(k + 1).charAt(col) == empty) {
-                  chars = fullPuzzleInput.get(k).toCharArray();
-                  char[] belowChars = fullPuzzleInput.get(k + 1).toCharArray();
-                  belowChars[col] = roundedRock;
-                  chars[col] = empty;
-                  fullPuzzleInput.set(k, new String(chars));
-                  fullPuzzleInput.set(k + 1, new String(belowChars));
-                  k++;
-                }
-              }
-            }
-          }
-//          for (String inputLine : fullPuzzleInput) {
-//            System.out.println(inputLine);
-//          }
-        }
-
-        // now shift right all roundedRocks until they hit either another roundedRock or a cubeRock
-        if (r%4 == 3) {
-//          System.out.println("RIGHT");
-          for (int l = 0; l < fullPuzzleInput.size(); l++) {
-            String singleLine = fullPuzzleInput.get(l);
-            char[] chars = singleLine.toCharArray();
-            for (int i = chars.length - 1; i >= 0; i--) {
-              if (chars[i] == roundedRock) {
-                int j = i;
-                while (j < chars.length - 1 && chars[j + 1] == empty) {
-                  chars[j + 1] = roundedRock;
-                  chars[j] = empty;
-                  j++;
-                }
-              }
-            }
-            fullPuzzleInput.set(l, new String(chars));
-          }
-//          for (String inputLine : fullPuzzleInput) {
-//            System.out.println(inputLine);
-//          }
-        }
+        System.out.println("Intermediate Result: " + step + ": " + hashResult);
+        result += hashResult;
       }
-
-      // DONE
-      System.out.println("DONE");
-      for (int i = 0; i < fullPuzzleInput.size(); i++) {
-        System.out.println("size: " + fullPuzzleInput.size());
-        String inputLine = fullPuzzleInput.get(i);
-        System.out.println(inputLine);
-        results += calculateSingleLineWeight(inputLine.toCharArray(), fullPuzzleInput.size()-i);
-      }
-      System.out.println("results: " + results);
-
+      System.out.println("Final Result: " + result);
     } catch (IOException e) {
       e.printStackTrace();
     }
-  }
-
-  /*
-   * This method should calculate the weight of a single line, the weight is calculated by taking the sum of the
-   *  distance from each roundedRock to the end of the string
-   */
-  public static long calculateSingleLineWeight(char[] chars, int weight) {
-    long totalWeight = 0;
-    for (int i = 0; i < chars.length; i++) {
-      if (chars[i] == roundedRock) {
-        totalWeight += weight;
-//        System.out.println("weight: " + weight);
-      }
-    }
-    return totalWeight;
   }
 }
 
