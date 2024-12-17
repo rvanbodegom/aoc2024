bf53c3e45c4d6179efb84ed46572776a9fed0212
Ruben van Bodegom <ruben@rubyit.nl>
2024-12-17 10:20:45 +0100

    Day 1

--- src/Main.java
+++ src/Main.java
@@ -5,58 +5,44 @@ import java.util.*;
 
 
 public class Main {
-  static char roundedRock = 'O';
-  static char cubeRock = '#';
-  static char empty = '.';
   static long result = 0;
-  static long nrOfRotations = 1000;
-
 
   public static void main(String[] args) {
     String filePath = "src/input.txt";
-    List<List<String>> puzzles = new ArrayList<>();
 
     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
       String line;
-      int hashResult = 0;
+      List<Integer> leftSide = new ArrayList<>();
+      List<Integer> rightSide = new ArrayList<>();
       List<String> fullPuzzleInput = new ArrayList<>();
-      HashMap<Integer, List<String>> shiftedPuzzleInput = new HashMap<>();
+
       while ((line = br.readLine()) != null) {
         if (!line.isEmpty()) {
-          fullPuzzleInput.addAll(List.of(line.split(",")));
+          leftSide.add(Integer.parseInt(line.split("\\s+")[0]));
+          rightSide.add(Integer.parseInt(line.split("\\s+")[1]));
+          //fullPuzzleInput.addAll(List.of(line.split(",")));
           System.out.println(fullPuzzleInput);
         }
       }
-      // boxnr, label, focalLength
-      List<Map><String, Integer>[] lenses = new LinkedHashMap<>();
-      for (String step : fullPuzzleInput) {
-        hashResult = 0;
-        String label;
-        if (step.contains("=")) {
-          label = step.split("=")[0];
-          for (char c : label.toCharArray()) {
-            hashResult += (int) c;
-          }
-          hashResult *= 17;
-          hashResult = hashResult % 256;
-          // add to box
-          lenses.put(hashResult, new Lens(label, Integer.parseInt(step.split("=")[1])));
-        } else { // "-"
-          hashResult = 0;
-          label = step.split("-")[0];
-          for (char c : label.toCharArray()) {
-            hashResult += (int) c;
-          }
-          hashResult *= 17;
-          hashResult = hashResult % 256;
-          if (lenses.containsKey(hashResult)) {
-            Lens tmp = lenses.get(hashResult); // get the lens
-            if (tmp.)
-            lenses.remove();
-          }
-        }
+      System.out.println("Left Side: " + leftSide);
+      System.out.println("Right Side: " + rightSide);
+      Collections.sort(leftSide);
+      Collections.sort(rightSide);
+      System.out.println("Left Side: " + leftSide);
+      System.out.println("Right Side: " + rightSide);
+      // calculate the distance between each element of leftSide and rightSide
+      for (int i = 0; i < leftSide.size(); i++) {
+        //result += Math.abs(leftSide.get(i) - rightSide.get(i));
+      }
+      for(int left: leftSide) {
+        // count the number of occurences of this number in the right side
+        result += left*Collections.frequency(rightSide, left);
+        System.out.println("Left: " + left + " Right: " + Collections.frequency(rightSide, left));
 
       }
+
+
+
       System.out.println("Final Result: " + result);
     } catch (IOException e) {
       e.printStackTrace();
