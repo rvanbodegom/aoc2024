a076027f3daefe6e54f5f4f5eb8b88a9196fd6a9
Ruben van Bodegom <ruben@rubyit.nl>
2024-12-20 09:44:03 +0100

    Day 5b - incomplete / error

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,7 @@ public class Main {
       String line;
       Map<Integer, List<Integer>> pageRules = new HashMap<>();
       List<List<Integer>> updates = new ArrayList<>();
+      List<List<Integer>> incorrectUpdates = new ArrayList<>();
       int lineNo = 0;
       boolean firstSection = true;
       while ((line = br.readLine()) != null) {
@@ -43,45 +44,70 @@ public class Main {
           continue;
         }
       }
-      System.out.println("Page Rules: " + pageRules);
-      System.out.println("Updates: " + updates);
+      for (Map.Entry<Integer, List<Integer>> entry : pageRules.entrySet()) {
+        List<Integer> tmp = entry.getValue();
+        Collections.sort(tmp);
+        System.out.println("Key: " + entry.getKey() + " Value: " + tmp);
+      }
+
+      //System.out.println("Page Rules: " + pageRules.sort());
+//      System.out.println("Updates: " + updates);
       // for each update check if the pagerules apply
+      boolean reset = true;
+      boolean identicalResult = true;
       for (List<Integer> update : updates) {
         boolean ruleApplies = true;
         for (int i = 0; i < update.size() - 1; i++) {
+          if (reset && !identicalResult) {
+            i = 0;
+            reset = false;
+            identicalResult = true;
+          }
           Integer key = update.get(i);
-          System.out.println("Key: " + key);
+//          System.out.println("Key: " + key + " with I: " + i);
           for (int j = i + 1; j < update.size(); j++) {
             if (pageRules.get(key) == null) {
-              System.out.println("Rule does not apply for update: " + update);
-//              System.out.println("=== Key: " + key + " does not exist in pageRules");
+//              System.out.println("Rule does not apply for update: " + update);
+              System.out.println("=== Key: " + key + " does not exist in pageRules");
+              // key has to be the last element in the update
+              update.remove(key);
+              update.add(key);
+
               ruleApplies = false;
+              identicalResult = false;
               break;
             }
             if (!pageRules.get(key).contains(update.get(j))) {
-              System.out.println("Rule does not apply for update: " + update);
+//              System.out.println("Original Rule does not apply for update: " + update + " key  " + key + " and " + update.get(j));
+              // swap positions
+              int tmp = update.get(i);
+              update.set(i, update.get(j));
+              update.set(j, tmp);
+
               ruleApplies = false;
+              identicalResult = false;
               break;
+            } else {
+              //System.out.println("== Rule applies for update: " + update + " key  " + key + " and " + update.get(j));
+              reset = true;
             }
           }
-//
-//          List<Integer> rules = pageRules.get(i);
-//          if (rules != null) {
-//            if (!rules.contains(update.get(i))) {
-//              System.out.println("Rule does not apply for update: " + update);
-//              ruleApplies = false;
-//              break;
-//            }
-//          }
         }
-        if (ruleApplies) {
-          // find the middle element of the update rules
-          int middle = update.get(update.size() / 2);
-          System.out.println("Middle: " + middle);
-          result+= middle;
+        if (!ruleApplies) {
+          incorrectUpdates.add(update);
+          System.out.println("Roel does not apply for update: " + update);
+
         }
       }
+      //System.out.println("Incorrect Updates: " + incorrectUpdates);
+      for (List<Integer> update : incorrectUpdates) {
+        // set in the correct order
 
+
+        int middle = update.get(update.size() / 2);
+        System.out.println("Middle: " + middle);
+        result += middle;
+      }
       System.out.println("Final Result: " + result);
 
     } catch (
